b97ca2bc13e8621a62e3c09f056f7218e68ba6b4
Per Mathisen <per.mathisen@arm.com>
2026-05-23 20:01:42 +0200

    Add glm as external submodule

--- scripts/setup_vkmark.sh
+++ scripts/setup_vkmark.sh
@@ -2,7 +2,7 @@
 
 # If you are not using Ubuntu, you need to figure out these yourself
 if [ -n "$(uname -a | grep Ubuntu)" ]; then
-	sudo apt install meson libglm-dev libassimp-dev libxcb1-dev libxcb-icccm4-dev libwayland-dev wayland-protocols libdrm-dev libgbm-dev
+	sudo apt install meson libassimp-dev libxcb1-dev libxcb-icccm4-dev libwayland-dev wayland-protocols libdrm-dev libgbm-dev
 fi
 
 git clone https://github.com/vkmark/vkmark.git external/vkmark
